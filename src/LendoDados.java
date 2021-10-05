ff06c3f86fa654e799e4c9f979db7557e9158e8d
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-10-05 20:18:00 -0300

    Adicionando comentários no código

--- src/LendoDados.java
+++ src/LendoDados.java
@@ -4,15 +4,20 @@ public class LendoDados {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//As duas linhas abaixo exibem mensagem na tela
 		System.out.println("Programa Educado");
-		System.out.println("Por favor digite o seu nome: ");
+		System.out.print("Por favor digite o seu nome: ");
 		
+		//Para ler dados a linha abaixo cria um Scanner
 		Scanner leitor = new Scanner(System.in);
+		
+		//Para guardar dados é necesário criar uma variável
 		String nome;
+		//Para ler dados e guardar na variável usamos o Scanner criado
 		nome = leitor.next();
 		
+		//A linha abaixo exhibe a mensagem com o conteúdo da variável
 		System.out.println("Seja Bem vindo(a) " + nome);
-		leitor.close();
-			
+		leitor.close();	
 	}
 }
