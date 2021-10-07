7654aaba6400a945adae35eb467a56dabcfe2aeb
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-10-06 21:08:37 -0300

    Criada calculadora que multiplica e divide dois valores

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -11,6 +11,8 @@ public class Calculadora {
 		int valor2;
 		int soma;
 		int subtracao;
+		int multiplicacao;
+		double divisao;
 		
 		
 		System.out.println("Por favor digite o seu primeiro número: ");
@@ -21,9 +23,14 @@ public class Calculadora {
 		
 		soma = valor1 + valor2;
 		subtracao = valor1 - valor2;
+		multiplicacao = valor1 * valor2;
+		divisao = valor1/(double)valor2;
 		
 		System.out.println("O valor da soma é: " + soma);
 		System.out.println("O valor da subtracão é: " + subtracao);
+		System.out.println("O valor da multiplicação é: " + multiplicacao);
+		System.out.println("O valor da divisão é: " + divisao);
+		
 		
 		
 	}
