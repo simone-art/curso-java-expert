3c001955fe4ccd2ef1ccd0334b6b5330d78eae50
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-10-06 21:01:10 -0300

    Criada calculadora que soma dois valores

---
+++ src/Calculadora.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Programa Calculadora");
+		Scanner leitor = new Scanner(System.in);
+		
+		int valor1;
+		int valor2;
+		int soma;
+		int subtracao;
+		int multiplicacao;
+		int divisao;
+		
+		System.out.println("Por favor digite o seu primeiro número: ");
+		valor1 = leitor.nextInt();
+		
+		System.out.println("Por favor digite o seu segundo número: ");
+		valor2 = leitor.nextInt();
+		
+		soma = valor1 + valor2;
+		
+		System.out.println("O valor da soma é: " + soma);
+		
+		
+	}
+
+}
